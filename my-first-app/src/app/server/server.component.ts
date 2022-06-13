39e11b6a572b4d53d6575cfd29f16cc8f2b96982
sabbir rahman <sabbir.rahman@kaz.com.bd>
2022-06-13 13:06:59 +0600

    dynamically changing the css

--- my-first-app/src/app/server/server.component.ts
+++ my-first-app/src/app/server/server.component.ts
@@ -9,7 +9,15 @@ export class ServerComponent {
     ServerId: number = 10;
     ServerStatus: string = 'running';
 
+    constructor() {
+        this.ServerStatus = Math.random() > 0.5 ? 'online' : 'offline'
+    }
+
     getDoubleServerId () {
         return this.ServerId * 2
+    }
+
+    getColor() {
+        return this.ServerStatus === 'online' ? 'green' : 'red'
     }
 }
