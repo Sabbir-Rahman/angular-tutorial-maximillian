208642e6964aac0354267f4e02b394e74bf810e6
Md Sabbir Rahman <sabbirrahman42@iut-dhaka.edu>
2022-06-15 11:20:17 +0600

    app is broken for splitting

--- components-databinding/src/app/app.component.ts
+++ components-databinding/src/app/app.component.ts
@@ -11,19 +11,5 @@ export class AppComponent {
   newServerName = '';
   newServerContent = ''
 
-  onAddServer() {
-    this.serverElements.push({
-      type: 'server',
-      name: this.newServerName,
-      content: this.newServerContent
-    })
-  }
-
-  onAddBlueprint() {
-    this.serverElements.push({
-      type: 'blueprint',
-      name: this.newServerName,
-      content: this.newServerContent
-    })
-  }
+ 
 }
