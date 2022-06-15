fd2498c441b05de38a63b74debfb9ef873f59983
Md Sabbir Rahman <sabbirrahman42@iut-dhaka.edu>
2022-06-15 11:14:27 +0600

    components and data binding

---
+++ components-databinding/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { CockpitComponent } from './cockpit/cockpit.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CockpitComponent
+  ],
+  imports: [
+    BrowserModule, FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
