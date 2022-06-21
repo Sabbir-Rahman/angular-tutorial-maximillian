9be17440f2b618bd1619581c8b6dd10f9d99ef35
Md Sabbir Rahman <sabbirrahman42@iut-dhaka.edu>
2022-06-21 06:06:34 +0600

    binding to custom events output

--- components-databinding/src/app/app.component.ts
+++ components-databinding/src/app/app.component.ts
@@ -9,5 +9,20 @@ export class AppComponent {
   title = 'components-databinding';
   serverElements = [{ type: 'server', name: 'TestServer', content: 'Just a test'}];
   
+  onServerAdded(serverData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    })
+  }
+
+  onBlueprintAdded(blueprintData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
+    })
+  }
  
 }
